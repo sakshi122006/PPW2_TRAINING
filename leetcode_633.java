9957c43bce6003c793625c2e73da3dbf2d2081c3
Sakshi Revaje <revajesakshi@gmail.com>
2026-07-29 15:30:56 +0530

    Create leetcode_633.java

---
+++ leetcode_633.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean judgeSquareSum(int c) 
+    {
+        int left = 0;
+        long right = (long)Math.sqrt(c);
+        while(left<=right)
+        {
+            long sum = left*left + right*right;
+            if(sum==c)
+            {
+                return true;
+            }
+            else if(sum<c)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return false;
+    }
+}
