5d8741a3a23887b86155abb39f05dd5a51792068
Sakshi Revaje <revajesakshi@gmail.com>
2026-07-30 14:30:37 +0530

    Implement subsetXORSum method in Solution class

---
+++ leetcode_1863.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int or = 0;
+
+        for (int num : nums) {
+            or |= num;
+        }
+
+        return or << (nums.length - 1);
+    }
+}
