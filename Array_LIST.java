5e0db128b19ec90b56cd9eb8ff0bddf82435187e
Sakshi Revaje <revajesakshi@gmail.com>
2026-07-30 14:43:24 +0530

    Add files via upload

---
+++ Array_LIST.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;public class Array_LIST {
+    public static void main(String[] args) {
+        ArrayList<String> std = new ArrayList<>();
+
+        
+        std.add("Ram");
+        std.add("Sham");
+        std.add("Arun");
+        std.add("Varun");
+        std.add("Ankush");
+        // Print the ArrayList
+        System.out.println("Students: " + std);
+
+        
+    }
+}
