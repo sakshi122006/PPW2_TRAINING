e801b14916577f024d9f42cf920781f415d5db4a
Sakshi Revaje <revajesakshi@gmail.com>
2026-09-06 14:07:18 +0530

    Create leetcode_367.java

---
+++ leetcode_367.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int odd=1;
+        while(num>0)
+        {
+            num-=odd;
+            odd+=2;
+        } 
+    return num == 0;   
+    }
+}
+
